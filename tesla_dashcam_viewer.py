47722bbdd68629f11a4eb86fc8dc331e4f8c34b4
Adam Chrystie <adamchry@gmail.com>
2025-01-01 14:15:49 -0800

    - Added a grid layout to hold four video playback widgets. - Made VideoEventWidget , a class which represents a single event in time and holds the media related info to control the 4 media widgets. - Updated the file chooser to support selecting multiple files since we want 4 inputs. Later i'll update this so we select one event's view and the others are found automatically.

--- tesla_dashcam_viewer.py
+++ tesla_dashcam_viewer.py
@@ -1,12 +1,12 @@
 import sys
 from PySide6.QtWidgets import (
-    QSizePolicy, QApplication, QWidget, QLayout, QVBoxLayout, QHBoxLayout,
+    QSizePolicy, QApplication, QWidget, QLayout, QGridLayout, QHBoxLayout, QVBoxLayout,
     QPushButton, QMainWindow, QFileDialog, QFrame)
 
 from PySide6.QtCore import Qt
 from PySide6.QtMultimedia import QMediaPlayer, QAudioOutput
 from PySide6.QtMultimediaWidgets import QVideoWidget
-from ui.video_widget import VideoWidget
+from ui.video_widget import VideoWidget, VideoEventWidget
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -14,31 +14,35 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Video Player Tool")
         self.setGeometry(0, 0, 1000, 600)
         #self.setStyleSheet("background-color: lightblue;")
+        self._camera_names = ['back', 'front', 'left', 'right']
 
+        self.media_player_video_widget_dict = {}
         # Main layout
         main_widget = QWidget()
         main_layout = QHBoxLayout()
 
         # Video display area
-        self.video_widget = QVideoWidget()
-        #self.video_widget.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-        self.video_widget.setAspectRatioMode(Qt.AspectRatioMode.KeepAspectRatio)
-        #self.video_widget.setStyleSheet("border: none;")
+
         video_widget_frame = QFrame()
         video_widget_frame.setFrameShape(QFrame.Box)
         video_widget_frame.setLineWidth(3)
-        frame_layout = QHBoxLayout(video_widget_frame)
+        #frame_layout = QHBoxLayout(video_widget_frame)
+        frame_layout = QGridLayout(video_widget_frame)
         frame_layout.setContentsMargins(0, 0, 0, 0)
         #frame_layout.setSpacing(0)
-        frame_layout.addWidget(self.video_widget, stretch=1)
+        for count, camera_name in enumerate(self._camera_names):
+            video_widget = QVideoWidget()
+            video_widget.setAspectRatioMode(Qt.AspectRatioMode.KeepAspectRatio)
+            frame_layout.addWidget(video_widget, count // 2, count % 2)
+            media_player = QMediaPlayer()
+            audio_output = QAudioOutput()
+            audio_output.setMuted(True)
+            media_player.setAudioOutput(audio_output)
+            media_player.setVideoOutput(video_widget)
+            self.media_player_video_widget_dict[camera_name] = {'video_widget': video_widget, 'media_player': media_player,
+                                                 'audio_output': audio_output}
         video_widget_frame.setLayout(frame_layout)
-        self.media_player = QMediaPlayer()
-        self.audio_output = QAudioOutput()
-        self.media_player.setAudioOutput(self.audio_output)
-        self.media_player.setVideoOutput(self.video_widget)
-        
         main_layout.addWidget(video_widget_frame, 3)
-
         # Video clip list column
         self.clip_list = QVBoxLayout()
         self.clip_list.setSizeConstraint(QLayout.SetFixedSize)
@@ -59,15 +63,23 @@ class MainWindow(QMainWindow):
 
     def add_video(self):
         file_dialog = QFileDialog(self)
-        file_dialog.setNameFilter("Videos (*.mp4 *.avi *.mkv)")
-        file_dialog.setFileMode(QFileDialog.ExistingFile)
+        video_files, _ = QFileDialog.getOpenFileNames(
+            None, "Select Files", "", "All Files (*);;Text Files (*.txt);;Python Files (*.py)")
+        if video_files:
+            self.add_video_clip_widget(video_files)
+            print(f'Added video clip widget for:{','.join(video_files)}')
 
-        if file_dialog.exec():
-            video_file = file_dialog.selectedFiles()[0]
-            self.add_video_clip_widget(video_file)
+        #file_dialog.setFileMode(QFileDialog.ExistingFile)
+        #file_dialog.setOption(QFileDialog.DontUseNativeDialog, True)
+        #file_dialog.setViewMode(QFileDialog.Detail)
+        #file_dialog.setNameFilter("Videos (*.mp4 *.avi *.mkv)")
+        # if file_dialog.exec():
+        #     video_files = file_dialog.selectedFiles()
+        #     self.add_video_clip_widget(video_files)
 
-    def add_video_clip_widget(self, video_file):
-        video_clip_widget = VideoWidget(video_file, self.media_player)
+    def add_video_clip_widget(self, video_files):
+        #video_clip_widget = VideoWidget(video_file, self.media_player)
+        video_clip_widget = VideoEventWidget("test", self.media_player_video_widget_dict, video_files)
         self.clip_list_widget.addWidget(video_clip_widget)
 
 if __name__ == "__main__":
--- ui/video_widget.py
+++ ui/video_widget.py
@@ -2,6 +2,114 @@ from PySide6.QtWidgets import (QWidget, QHBoxLayout, QLabel, QPushButton)
 from PySide6.QtMultimedia import QMediaPlayer
 from PySide6.QtCore import QUrl
 
+
+class VideoEventWidget(QWidget):
+    """A single multi view video event to represent a specific time."""
+    def __init__(self, event_name: str, media_video_players: dict, video_files: list):
+        super().__init__()
+        self._is_playing = False
+        self._event_name = event_name
+        self._backup_player = media_video_players['back']['media_player']
+        self._front_upper_player = media_video_players['front']['media_player']
+        self._left_repeater_player = media_video_players['left']['media_player']
+        self._right_repeater_player = media_video_players['right']['media_player']
+        self._video_files = video_files
+        self.setup_ui()
+
+    @property
+    def event_name(self):
+        return self._event_name
+    @event_name.setter
+    def event_name(self, value):
+        self._event_name = value
+
+    def setup_ui(self):
+        # Set up layout
+        layout = QHBoxLayout()
+        layout.setSpacing(3)
+        layout.setContentsMargins(2, 4, 2, 4)
+
+        # Label to display the video file name
+        self.label = QLabel(self.event_name)
+        layout.addWidget(self.label)
+
+        # Play/Pause button
+        self.play_pause_button = QPushButton("Play/Pause")
+        self.play_pause_button.clicked.connect(self.toggle_play_pause)
+        layout.addWidget(self.play_pause_button)
+
+        self.setLayout(layout)
+        self.set_style()
+
+    def set_style(self):
+        """Apply a stylesheet."""
+
+        qml = """
+        QWidget {
+                background-color: #f0f0f0;
+                border-radius: 10px;
+                border: 0px solid #d0d0d0;
+            }
+        QLabel {
+            font-size: 14px;
+            font-weight: normal;
+            background-color: #0078d7;
+            color: white;
+        }
+        QPushButton {
+            background-color: #0078d7;
+            color: white;
+            border: none;
+            border-radius: 5px;
+            padding: 5px 10px;
+        }
+        QPushButton:hover {
+            background-color: #005bb5;
+        }
+        """
+
+        self.setStyleSheet(qml)
+
+    def toggle_play_pause(self):
+        """
+        self._backup_player = media_video_players['back']
+        self._front_upper_player = media_video_players['front']
+        self._left_repeater_player = media_video_players['left']
+        self._right_repeater_player = media_video_players['right']
+        :param self:
+        :return:
+        """
+        if self._is_playing:
+            self._backup_player.pause()
+            self._front_upper_player.pause()
+            self._left_repeater_player.pause()
+            self._right_repeater_player.pause()
+            self.play_pause_button.setText("Play")
+        else:
+            self._backup_player.setSource(QUrl.fromLocalFile(self._video_files[0]))
+            self._front_upper_player.setSource(QUrl.fromLocalFile(self._video_files[1]))
+            self._left_repeater_player.setSource(QUrl.fromLocalFile(self._video_files[2]))
+            self._right_repeater_player.setSource(QUrl.fromLocalFile(self._video_files[3]))
+            self._backup_player.play()
+            self._front_upper_player.play()
+            self._left_repeater_player.play()
+            self._right_repeater_player.play()
+            self.play_pause_button.setText("Pause")
+
+        self._is_playing = not self._is_playing
+
+        # if self.media_player.source().toLocalFile() != self.video_file:
+        #     self.media_player.setSource(QUrl.fromLocalFile(self.video_file))
+        #     self.media_player.play()
+        #     self.play_pause_button.setText("Pause")
+        # elif self.media_player.playbackState() == QMediaPlayer.PlayingState:
+        #     self.media_player.pause()
+        #     self.play_pause_button.setText("Play")
+        # else:
+        #     self.media_player.play()
+        #     self.play_pause_button.setText("Pause")
+
+
 class VideoWidget(QWidget):
     def __init__(self, video_file, media_player):
         super().__init__()
