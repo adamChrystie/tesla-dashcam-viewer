c092d60e552d798a7bbf781e11057dbb3b366c82
Adam Chrystie <adamchry_git@atomfx.com>
2025-02-25 21:28:11 -0800

    1) Made settings.py which has a basic settings class for manipulating a json file used to store settings. Though I could use QSettings later. 2) When app opens settings file is read and the settings instance is updated. If no settings file exists, a fresh one is made. 3) When app closes, settings instance writes an updated json file. 4) Eventually the 24 hour update check mechanism will use the json file's data to determine if a check should happen.

--- constants.py
+++ constants.py
@@ -4,12 +4,6 @@ import platform
 
 API_URL = "https://www.adamchrystie.com/tesla-dashcam-viewer/api"
 APP_VERSION = 'v1.0.1'
-APP_TMP_DATA_FOLDER = 'tesla_dashcam_viewer'
+APP_SETTINGS_DATA_FOLDER = 'tesla_dashcam_viewer'
+APP_SETTINGS_FILE_NAME = "tesla_dashcam_viewer_settings.json"
 TESLAS_CAMERA_NAMES = ['back', 'front', 'left_repeater', 'right_repeater']
-
-if platform.system() == 'Windows':
-    TMP_DATA_DIR = os.path.join(os.getenv("APPDATA"), APP_TMP_DATA_FOLDER)
-elif platform.system() == 'Darwin':
-    TMP_DATA_DIR = os.path.join(os.getenv("HOME"), "Library", "Application Support", APP_TMP_DATA_FOLDER)
-else:
-    TMP_DATA_DIR = None
--- file_utils/updates.py
+++ file_utils/updates.py
@@ -1,11 +1,29 @@
 import os
+from pathlib import Path
 import datetime
 import json
 import platform
 from typing import Union
 import requests
-from constants import APP_TMP_DATA_FOLDER, APP_VERSION, API_URL
+from constants import APP_VERSION, API_URL
 
+
+def is_time_to_check(self, last_time_checked: datetime.datetime) -> bool:
+    """Check for updates once per day and display a message if there is one."""
+    check_for_update = False
+    current_datatime = datetime.datetime.now().isoformat()
+
+    return check_for_update
+
+def get_last_time_checked(last_check_fpath: Union[str, Path]) -> Union[str, Path, None]:
+    """Get the last time the app checked to see if there was a new version available."""
+
+    last_updated_jason_fpath = file_utils.updates.get_app_settings_file_path()
+    if os.path.exists(last_updated_jason_fpath):
+        # read it and see if it has been 24 hours
+        pass
+    else:
+        file_utils.updates.write_json_file(last_updated_jason_fpath, data_dict)
 
 def should_check_for_update(last_check_timestamp):
     # Convert the stored timestamp to a datetime object
@@ -15,16 +33,6 @@ def should_check_for_update(last_check_timestamp):
     # Check if 24 hours have passed
     return now >= last_check_time + datetime.timedelta(days=1)
 
-def get_update_check_path():
-    if platform.system() == "Windows":  # Windows
-        folder = os.path.join(os.getenv("APPDATA"), APP_TMP_DATA_FOLDER)  # Roaming folder
-    elif platform.system() == 'Darwin':  # macOS
-        folder = os.path.join(os.path.expanduser("~"), "Library", "Application Support", "YourApp")
-    else:
-        return None
-
-    os.makedirs(folder, exist_ok=True)  # Ensure folder exists
-    return os.path.join(folder, "last_update_check.json")
 
 def check_for_new_version(current_version: str=None) -> Union[str, None]:
     """ Check to see if there is a newer version and return the version string or None."""
@@ -40,6 +48,3 @@ def check_for_new_version(current_version: str=None) -> Union[str, None]:
         print("WARNING: Could not check for updates.")
     return None
 
-def write_json_file(fpath: str, data: dict):
-    with open(fpath, 'w') as f:
-        json.dump(data, f)
